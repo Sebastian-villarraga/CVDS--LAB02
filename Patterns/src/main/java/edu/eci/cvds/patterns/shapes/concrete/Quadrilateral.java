7df7180dd5963967edfc7172f3d7a757a08ea1b7
Andres Jacobo <andres.sepulveda-s@mail.escuelaing.edu.co>
2025-01-29 15:51:22 -0500

    Agrego codigo faltante

--- Patterns/src/main/java/edu/eci/cvds/patterns/shapes/concrete/Quadrilateral.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/shapes/concrete/Quadrilateral.java
@@ -2,9 +2,8 @@ package edu.eci.cvds.patterns.shapes.concrete;
 
 import edu.eci.cvds.patterns.shapes.Shape;
 
-public class Triangle implements Shape{
+public class Quadrilateral implements Shape {
     public int getNumberOfEdges() {
-
-        return 3;
+        return 4;
     }
 }
